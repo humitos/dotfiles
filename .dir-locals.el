cb84dde37feea4a7b5d491eddadc568050b79d7d
Manuel Kaufmann <humitos@gmail.com>
2016-11-30 15:52:58 -0500

    Emacs default fixes

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,18 +1,17 @@
-;; default .dir-locals.el to use in HOME directory
 ((python-mode
   . (
-     (fill-column . 79)
+     ;; This is used to fill the paragraph in 80 columns when pressing M-q
+     (fill-column . 80)
 
      ;; Use 4 spaces to indent in Python
      (python-indent-offset . 4)
-     ;; autopep8 global configurations
-     (py-autopep8-options . ("--global-config=~/.emacs.d/autopep8rc"))
+     ;; autopep8 global configurations (path must be absolute)
+     (py-autopep8-options . ("--global-config=/home/humitos/.autopep8rc"))
      ;; FlyCheck global configurations
-     (flycheck-flake8rc . "~/.emacs.d/flake8rc")
+     (flycheck-flake8rc . "/home/humitos/.flake8rc")
 
      ;; docformatter global options
-     (eval . (setq py-docformatter-options
-		   '("--wrap-summaries=120" "--wrap-descriptions=120" "--pre-summary-newline" "--no-blank")))
+     (py-docformatter-options . ("--wrap-summaries=80" "--wrap-descriptions=80" "--pre-summary-newline" "--no-blank"))
 
      ;; use our own virtualenv for this project
      (eval . (pyvenv-workon 'emacs-default))
