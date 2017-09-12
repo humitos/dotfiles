56feed20be6a87c257f95df4d9cb23f377d15ff5
Manuel Kaufmann <humitos@gmail.com>
2017-09-12 18:51:45 -0500

    Improve switch monitor to use settings

--- bin/switch-monitor.sh
+++ bin/switch-monitor.sh
@@ -1,11 +1,49 @@
+# $ xrandr
+# Screen 0: minimum 8 x 8, current 1920 x 1080, maximum 32767 x 32767
+# eDP1 connected 1920x1080+0+0 (normal left inverted right x axis y axis) 310mm x 170mm
+#    1920x1080     60.05*+
+#    1400x1050     59.98
+#    1600x900      60.00
+#    1280x1024     60.02
+#    1280x960      60.00
+#    1368x768      60.00
+#    1280x720      60.00
+#    1024x768      60.00
+#    1024x576      60.00
+#    960x540       60.00
+#    800x600       60.32    56.25
+#    864x486       60.00
+#    640x480       59.94
+#    720x405       60.00
+#    640x360       60.00
+# DP1 disconnected (normal left inverted right x axis y axis)
+# DP2 disconnected (normal left inverted right x axis y axis)
+# HDMI1 connected (normal left inverted right x axis y axis)
+#    1920x1080     60.00 +  50.00    59.94
+#    1920x1080i    60.00    50.00    59.94
+#    1680x1050     59.88
+#    1280x1024     60.02
+#    1280x960      60.00
+#    1152x864      59.97
+#    1280x720      60.00    50.00    59.94
+#    1024x768      60.00
+#    800x600       60.32
+#    720x576       50.00
+#    720x480       60.00    59.94
+#    640x480       60.00    59.94
+# VIRTUAL1 disconnected (normal left inverted right x axis y axis)
 
 CURRENT_MONITOR=`xrandr | grep 1920x1080+0+0 | awk '{ print $1 }'`
 
-if [ $CURRENT_MONITOR = "HDMI-1" ]
+HDMI="HDMI1"
+NOTEBOOK="eDP1"
+
+
+if [ $CURRENT_MONITOR = $HDMI ]
 then
     # switch to NOTEBOOK monitor
-    xrandr --output HDMI-1 --off --output eDP-1 --mode 1920x1080 --pos 0x0 --rotate normal
+    xrandr --output $HDMI --off --output $NOTEBOOK --mode 1920x1080 --pos 0x0 --rotate normal
 else
     # switch to EXTERNAL (LG) monitor
-    xrandr --output eDP-1 --off --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal
+    xrandr --output $NOTEBOOK --off --output $HDMI --mode 1920x1080 --pos 0x0 --rotate normal
 fi
