629df631d67d4f58cff1412bd0da07d76d34c476
Manuel Kaufmann <humitos@gmail.com>
2025-10-26 20:00:39 +0100

    Add missing scripts I've been using forever

---
+++ bin/work-monitor.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Samsung UltraWide
+xrandr --output DP-3 --mode 3440x1440 --pos 0x0
+
+# Lenovo Monitor
+xrandr --output eDP-1 --off
+
+# External Monitor Arzopa
+xrandr --output DP-1 --mode 1920x1080 --pos 1520x1440
